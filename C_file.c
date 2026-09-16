a9f9ba8bcd6d579cef7fdf83b8bc33643b098344
NotDonQuixote <moralesf9825@gmail.com>
2026-09-16 13:56:49 -0600

    Updated .gitignore

---
+++ C_file.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int age = 22;
+    int height = 155;
+    float weight = 90.25;
+
+    printf("Age: %d\n", age);
+    printf("Height: %d cm\n", height);
+    printf("Weight: %.2f kg\n", weight);
+
+    return 0;
+
+}
---
+++ Lab2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int hex_to_binary(char a){
+    int hex_letter[6] = {1010, 1011, 1100, 1101, 1110, 1111};
+    int binary = 0; 
+    return 1; //rest goes here
+}
+
+int main() {
+    char hex[20];
+    printf("Please enter a hex code: ");
+    scanf("%s", hex);
+    printf("hex code test: %s", hex);
+    return 0;
+}
+
